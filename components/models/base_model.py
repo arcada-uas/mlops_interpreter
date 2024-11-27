d29ad49a498f243fc421f7c4d7b22259934b32d2
Shohel Rana <cse_shohel@yahoo.com>
2024-11-27 10:44:28 +0200

    refactor the base_model and added abstraction to force all method into implementation

--- components/models/base_model.py
+++ components/models/base_model.py
@@ -1,14 +1,28 @@
-class base_model:
+from abc import ABC, abstractmethod
+from pydantic import BaseModel as PydanticModel, Field
+
+
+class BaseModel(ABC):
+    class Config(PydanticModel):
+        alpha: float = Field(
+            ..., ge=0, description="Alpha must be a non-negative float"
+        )
+
+        def __init__(self, **kwargs):
+            super().__init__(**kwargs)
+
     def __init__(self, input_params: dict):
+        config = self.Config(**input_params)
+        self.alpha = config.alpha
 
-        # NON NEGATIVE FLOAT
-        self.alpha = input_params['alpha']
-
+    @abstractmethod
     def fit(self, features, labels=None):
-        raise NotImplementedError()
+        pass
 
+    @abstractmethod
     def predict(self, features):
-        raise NotImplementedError()
-    
+        pass
+
+    @abstractmethod
     def score(self, features, labels):
-        raise NotImplementedError()
+        pass
